60557fabb86490e6f6d3c6f87a02440ea3a25c4d
ByLuiZz <luis11esteban69@gmail.com>
2023-06-11 23:52:02 -0600

    Portafolio

---
+++ src/App.vue
@@ -0,0 +1,229 @@
+<template>
+  
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid justify-content-end">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#Contenidos">Contenidos</a>
+        <a class="nav-link" href="#apuntes">Apuntes</a>
+        <a class="nav-link " href="#practicas">Practicas</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    
+  <div class="banner-1">
+    <div class="flex items-center">
+  <div class="w-11/12 space-y-5 md:space-y-10 text-right">
+    <h4 class="font-sans">
+      Portafolio del computo III
+    </h4>
+    <h1 class="display-4">
+      Alumno:
+      <p class="display-3 pre"> Luis Esteban Blanco Diaz</p>
+    </h1>
+    <h4 class="font-bold text-purple-500">
+      Estudiante de la materia de Programacion Computacional IV de la carrera de Ingenieria en Sistemas y redes informaticas.
+    </h4>
+  </div>
+
+  <div class="w-8/12 flex justify-start">
+    <img
+      class="3x1:h-[500px] 2xl:h-[700px] xl:h-[550px] lg:h-[600px] md:h-[450px] sm:h-[350px]"
+      src="https://dei.uca.edu.sv/wp-content/uploads/bfi_thumb/logo-ugb-vertical-39ah4x52wqifdk1c84gu0w.png"
+      alt="user image"
+    />
+  </div>
+</div>
+
+  </div>
+
+
+  <div class="banner-2 space-y-10 pb-20" id="Contenidos">
+    <center><h3 class="heading3 my-5">CONTENIDOS VISTOS EN EL COMPUTO</h3></center>
+<div>
+  </div>
+    <div class="card mb-3" style="max-width: 1500px;">
+      <div class="row g-0">
+        <div class="col-md-4 d-flex justify-content-center align-items-center">
+         <img src="https://4.bp.blogspot.com/-42QJ6t_SM1s/V5QSINVMRYI/AAAAAAAACWU/CADS_pD-IE8gyn9GRaQupFX3NNEFsotpgCLcB/s1600/Estructura%2Blaravel.JPG" class="img-fluid rounded-start" alt="700">
+        </div>
+       <div class="col-md-8">
+          <div class="card-body">
+            <p class="card-text"><large class="text-muted">Semana 14</large></p>
+            <h5 class="card-title">Sistema de rutas y controladores</h5>
+            <p class="card-text">En Laravel, las rutas son las direcciones URL que los usuarios pueden visitar en una aplicación web. 
+            Permiten definir cómo se manejará una solicitud HTTP específica y qué acción se realizará en respuesta a esa solicitud. Los controladoresson clases que contienen la lógica de procesamiento para las diferentes rutas de una aplicación. 
+            Se utilizan para agrupar y organizar la lógica relacionada en un lugar centralizado, lo que facilita el mantenimiento y la reutilización del código.
+            En resumen, las rutas definen las URL y los controladores contienen la lógica para manejar esas rutas de manera organizada y reutilizable
+           </p>
+            <hr style="height:3px;border:none;color:black;background-color:black;" />
+            
+            <a href= "https://drive.google.com/file/d/1Xncmz8Mu878kpT5dF3XTIEOwA0bg-ou3/view?usp=sharing"
+            class="btn btn-info stretched-link">Contenido en PDF</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-3" style="max-width: 1500px;">
+      <div class="row g-0">
+        <div class="col-md-4 d-flex justify-content-center align-items-center">
+          <img src="https://kinsta.com/es/wp-content/uploads/sites/8/2023/05/laravel-validation-1024x512.jpg" class="img-fluid rounded-start" alt="700">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+          <p class="card-text"><large class="text-muted">Semana 15</large></p>
+          <h5 class="card-title">Validacion de datos</h5>
+          <p class="card-text">Las validaciones en Laravel se utilizan para garantizar la integridad y la validez de los datos ingresados por el usuario,
+            evitando así posibles errores o inconsistencias en la aplicación. Algunos ejemplos comunes de validaciones incluyen verificar que un campo obligatorio no esté vacío, validar el formato de un correo electrónico, comprobar que un número esté dentro de un rango específico, entre otros.
+          </p>
+          <hr style="height:3px;border:none;color:black;background-color:black;" />
+          <a href= "https://drive.google.com/file/d/1hI6Oa-GJMZYbsg_ei1HJzuN0T45UykmO/view?usp=sharing"
+          class="btn btn-info stretched-link">Contenido en PDF</a>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card mb-3" style="max-width: 1500px;">
+    <div class="row g-0">
+      <div class="col-md-4 d-flex justify-content-center align-items-center">
+        <img src="https://embed-ssl.wistia.com/deliveries/71b320f6f756ae4cc7b1450cf7bf8f7d.jpg" class="img-fluid rounded-start" alt="700">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <p class="card-text"><large class="text-muted">Semana 16</large></p>
+          <h5 class="card-title">API REST con laravel</h5>
+          <p class="card-text">Una API REST en Laravel es una interfaz de programación de aplicaciones que sigue los principios de la arquitectura REST y permite a los clientes comunicarse con un servidor para realizar operaciones CRUD en recursos expuestos. 
+            Laravel proporciona herramientas y características para facilitar el desarrollo de APIs RESTful de manera eficiente y estructurada.
+          </p>
+          <hr style="height:3px;border:none;color:black;background-color:black;" />
+          <a href= "https://drive.google.com/file/d/1xyAFJEvQwGZ3dbGfUC_dMN3MKDSjL-45/view?usp=sharing"
+          class="btn btn-info stretched-link">Contenido en PDF</a>
+        
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card mb-3" style="max-width: 1500px;">
+    <div class="row g-0">
+      <div class="col-md-4 d-flex justify-content-center align-items-center">
+        <img src="https://miro.medium.com/max/3000/1*3_7q354mTTlzbT_OMsDu9A.png" class="img-fluid rounded-start" alt="700">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <p class="card-text"><large class="text-muted">Semana 17 </large></p>
+          <h5 class="card-title">Autenticación API REST con Laravel</h5>
+          <p class="card-text">La autenticación de API REST en Laravel es el proceso de verificar la identidad y los permisos de un usuario o cliente que accede a la API. 
+            Laravel ofrece diversas opciones para implementar la autenticación, siendo los tokens de acceso una forma común de lograrlo.
+          </p>
+          <hr style="height:3px;border:none;color:black;background-color:black;" />
+          <a href= "https://drive.google.com/file/d/1aG52v8aY0Nznnrky7EEjTghoObcQuZ3O/view?usp=sharing"
+          class="btn btn-info stretched-link">Contenido en PDF</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card mb-3" style="max-width: 1500px;">
+    <div class="row g-0">
+      <div class="col-md-4 d-flex justify-content-center align-items-center">
+        <img src="https://jonmircha.com/img/blog/rest-crud.png" class="img-fluid rounded-start" alt="700">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <p class="card-text"><large class="text-muted">Semana 18</large></p>
+          <h5 class="card-title">Consumo de API REST</h5>
+          <p class="card-text"> El consumo de una API REST se refiere a la manera en que una aplicación o cliente utiliza y accede a los recursos y datos expuestos por dicha API.
+             En otras palabras, es el proceso de realizar solicitudes a través de una API REST para obtener, enviar, modificar o eliminar datos.
+            Cuando se consume una API REST, el cliente envía solicitudes HTTP a los endpoints (URLs) definidos en la API, utilizando los métodos estándar como GET, POST, PUT, PATCH o DELETE, según la operación que se desee realizar.
+          </p>
+          <hr style="height:3px;border:none;color:black;background-color:black;" />
+          <a href= "https://drive.google.com/file/d/1i5fJEzlBvf4UmOBlAm-nN5caydYxuv0Z/view?usp=sharing"
+          class="btn btn-info stretched-link">Contenido en PDF</a>
+        </div>
+      </div>
+    </div>
+</div>
+  <div class="card mb-3" style="max-width: 1500px;">
+    <div class="row g-0">
+      <div class="col-md-4 d-flex justify-content-center align-items-center">
+        <img src="https://programacionymas.com/images/series/laravel-oauth2.png" class="img-fluid rounded-start" alt="700">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <p class="card-text"><large class="text-muted">Semana 19</large></p>
+          <h5 class="card-title">Autenticacion por proveedores</h5>
+          <p class="card-text">Laravel proporciona un sistema de autenticación llamado "Socialite" que simplifica la integración de la autenticación por proveedores en una aplicación Laravel. 
+            Socialite maneja la comunicación y el flujo de autenticación con los proveedores de terceros, permitiendo que los usuarios autentiquen su identidad utilizando sus cuentas existentes en esos servicios.
+          </p>
+          <hr style="height:3px;border:none;color:black;background-color:black;" />
+          <a href= "https://drive.google.com/file/d/1fbStk4h6eeENvv1gp8hFEDjl0BB_C4n7/view?usp=sharing"
+          class="btn btn-info stretched-link">Contenido en PDF</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+  <div class="banner-1" id="apuntes">
+    <div class="space-y-4 grid place-items-center mt-5">
+      <h3 class="heading3">APUNTES</h3>
+      <center><p class="font-work_sans text-gray-600 font-semibold leading-relaxed">
+       De acuerdo a lo visto con laravel solo pude tomar apuntes de lo importante como los comandos para la creacion de controladores etc
+      </p></center>
+    </div>
+    <div class="card bg-dark ">
+  <img src="./assets/Apunte.jpeg" class="card-img" alt="...">
+</div>
+  </div>
+  <div class="banner-2 md:flex items-center md:justify-evenly" id="about">
+      <img
+        class="md:h-[400px] h-[400px]"
+        src="https://proximahost.es/blog/wp-content/uploads/2022/05/Laravel.jpg"
+        alt="user image"
+      />
+      <div class="space-y-5 py-8 px-8 md:py-16 md:px-20 md:w-1/2">
+        <h4 class="heading5 my-5" >Conocimientos Computo III</h4>
+        <p class="heading4 font-work_sans ">
+          Laravel es un framework de desarrollo web que he aprendido que proporciona un conjunto completo de herramientas y características para crear aplicaciones web robustas. 
+          Me he dado cuenta de que permite definir rutas y controladores, lo que ayuda a estructurar y manejar las solicitudes de los usuarios de manera organizada y reutilizable.
+          También he aprendido que Laravel incorpora un sistema de validaciones que asegura la integridad y validez de los datos ingresados por los usuarios en la aplicación. 
+          Esto es especialmente útil para evitar errores o inconsistencias en los datos.
+          Otra cosa que he aprendido es que Laravel facilita la creación de APIs REST, lo que permite a los clientes comunicarse y consumir datos desde un servidor de manera estructurada y eficiente. 
+          Esto es importante para desarrollar aplicaciones web modernas y escalables.
+          Finalmente, me he dado cuenta de que Laravel también admite la autenticación por proveedores externos, lo que permite a los usuarios utilizar sus credenciales existentes de servicios populares como Google o Facebook para iniciar sesión en la aplicación.
+          Esto es conveniente tanto para los usuarios como para los desarrolladores, ya que simplifica el proceso de autenticación y mejora la experiencia del usuario.
+       </p>
+        <div>
+      </div>
+      </div>
+    </div>
+    
+    <div class="card mb-3" style="max-width: 1500px;" id="practicas">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png" class="img-fluid rounded-start" alt="700">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">PRACTICAS</h5>
+            <p class="card-text">En este apartado esta el link para ir al repositorios con las practicas de este computo 
+            </p>
+            <hr style="height:5px;border:none;color:black;background-color:black;" />
+            <a href= ""
+            class="btn btn-info stretched-link">PRACTICAS</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+    
+    
+
+</template>
